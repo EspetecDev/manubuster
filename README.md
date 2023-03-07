dcbeb7fc16eccc01ee2030f42be7cd9ebadde433
Espetec <manutheking1991@gmail.com>
2023-03-07 10:13:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+current tutorial time
+https://youtu.be/-0exw-9YJBo?t=2070
+
+
+mongodb+srv://test:test2@cluster0.nls4cui.mongodb.net/?retryWrites=true&w=majority
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const port = process.env.port || 5000;
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler);
+app.listen(port, () => console.log(`server started on ${port}`));
